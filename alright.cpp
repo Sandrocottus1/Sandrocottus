32fe584d1847546e607a913ca03985ce90d4240f
Sandrocottus1 <74095206+Sandrocottus1@users.noreply.github.com>
2023-03-04 00:51:54 +0530

    Add files via upload

---
+++ alright.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+using namespace std;
+int main(){
+    for (int i = 0; i < 1; i++)
+    {
+        cout<<"AA"<<"         "<<"AA"<<"                        "<<"AA"<<"    "<<"AA"<<"      "<<"AAAAAAA"<<"    "<<"AAAAAAA"<<"       "<<"AAAAA"<<"  "<<"AAAAA"<<"   "<<"AA"<<"     "<<"AA\n";
+    }
+    for (int i = 0; i < 1; i++)
+    {
+        cout<<"AA"<<"         "<<"AA"<<"  "<<"AA"<<"  "<<"AA"<<"     "<<"AA"<<"         "<<"AA"<<"    "<<"AA"<<"      "<<"AA    "<<"AA"<<"    "<<"AA"<<"           "<<"AA"<<"   "<<"AA"<<"   "<<"AA"<<"   "<<"AA"<<"     "<<"AA\n";
+    }
+    for (int i = 0; i < 2; i++)
+    {
+        cout<<"AA"<<"   AA    "<<"AA"<<"  "<<"AA"<<"  "<<"AA"<<"     "<<"AA"<<"         "<<"AA"<<"    "<<"AA"<<"      "<<"AA   "<<"AA"<<"     "<<"AAAAA"<<"        "<<"AA"<<"        "<<"AA"<<"  "<<"  AA"<<"   "<<"AA\n";        
+    }
+    for (int i = 0; i < 2; i++)
+    {
+        cout<<"AA"<<"   AA    "<<"AA"<<"  "<<"AA"<<"  "<<"AA"<<"     "<<"AA"<<"         "<<"AA"<<"    "<<"AA"<<"      "<<"AA   "<<"  AA"<<"   "<<"AA"<<"           "<<"AA"<<"        "<<"AA"<<"       "<<"AA\n";        
+    }
+    for (int i = 0; i < 13; i++)
+    {
+        cout<<"A";
+    }
+    cout<<"  "<<"AA"<<"  ";
+    for (int i = 0; i < 6; i++)
+    {
+        cout<<"A";
+    }
+    cout<<" ";
+    for (int i = 0; i < 6; i++)
+    {
+        cout<<"A";
+    }   
+    cout<<"      ";
+    for (int i = 0; i < 6; i++)
+    {
+        cout<<"A";
+    }
+    cout<<"       ";
+    for (int i = 0; i < 8; i++)
+    {
+        cout<<"A";
+    }
+    cout<<"   ";
+    for (int i = 0; i < 8; i++)
+    {
+        cout<<"A";
+    }
+    cout<<"      ";
+    for (int i = 0; i < 2; i++)
+    {
+        cout<<"A";
+    }
+    cout<<"        ";
+    for (int i = 0; i < 2; i++)
+    {
+        cout<<"A";
+    }
+    cout<<"       AA";
+    
+    cout<<"\n";
+    cout<<"\n";
+    for (int i = 0; i < 1; i++)
+    {
+        cout<<"AAAAAAA"<<"    "<<"AAAAAAAA"<<"    "<<"AAAAAAAA"<<"  "<<"AAAAAAAA"<<"       "<<"AAAAAAA\n";
+    }
+    for (int i = 0; i < 2; i++)
+    {
+        cout<<"AA"<<"    "<<"AA"<<"   "<<"AA"<<"     "<<"AA"<<"   "<<"  AA"<<"      "<<"AA"<<"       "<<"          AA\n"; 
+    }
+    for (int i = 0; i < 1; i++)
+    {
+        cout<<"AA"<<"    "<<"AA"<<"   "<<"AA"<<" AAA "<<"AA"<<"   "<<"  AA"<<"      "<<"AAAAA"<<"         "<<"AAAAAAA\n"; 
+    } 
+    for (int i = 0; i < 2; i++)
+    {
+        cout<<"AA"<<"    "<<"AA"<<"   "<<"AA"<<"     "<<"AA"<<"   "<<"  AA"<<"      "<<"AA"<<"            "<<"AA\n"; 
+    }
+    cout<<"AAAAAA     AA     AA     AA     AAAAAAAA\n";
+    cout<<"                                               AA";
+    cout<<"\n";
+    cout<<"Your answer please ;)  ";
+    string a;
+    cin>>a;
+    if(a=="yes" or a=="yep" or a=="yup" or a=="YES" or a=="Yes" or a=="yES" or a=="YEs" or a=="YeS" or a=="hn" or a=="Han" or a=="Hn" or a=="hm"){
+        cout<<"Less go !!! X)!";
+    }
+    else{
+        cout<<"No problem we had a good time toghether ;) !";
+    }
+    
+    
+}
